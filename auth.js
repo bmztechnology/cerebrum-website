0ea61b433d2b1552711425d60356dd6618ab0fa0
CerebrumForex <cerebrum@forex.local>
2026-01-26 12:08:47 +0100

    feat: add Google and GitHub authentication

--- auth.js
+++ auth.js
@@ -2,10 +2,20 @@ import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./lib/db";
 import Nodemailer from "next-auth/providers/nodemailer";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: DrizzleAdapter(db),
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
         Nodemailer({
             server: {
                 host: process.env.SMTP_HOST,
