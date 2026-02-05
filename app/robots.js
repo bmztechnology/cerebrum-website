561bd1b5ae09f2da6fe883bfd627a6a8c6a3dd85
CerebrumForex <cerebrum@forex.local>
2026-02-05 12:02:33 +0100

    SEO: Disallow indexing of private dashboard routes in robots.js

--- app/robots.js
+++ app/robots.js
@@ -12,6 +12,7 @@ export default function robots() {
                     '/api/',
                     '/_next/',
                     '/private/',
+                    '/*/dashboard',
                 ],
             },
             {
